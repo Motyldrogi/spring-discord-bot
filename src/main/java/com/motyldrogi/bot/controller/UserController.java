ea5575a3d6675780c8348645fd86cfc39badcc94
Motyldrogi <motyldrogi@gmail.com>
2021-06-24 19:15:53 +0200

    Removed unused deps, changed api path

--- src/main/java/com/motyldrogi/bot/controller/UserController.java
+++ src/main/java/com/motyldrogi/bot/controller/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/v1/users")
+@RequestMapping("/api/users")
 @RestController
 public class UserController {
 
